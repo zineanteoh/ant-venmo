ae4a0fbb3e0c4d926176b99a88c5bf4c3d354342
nkalupahana <avishkank@gmail.com>
2024-09-05 17:43:59 -0700

    initial commit

---
+++ index.mjs
@@ -0,0 +1,51 @@
+import puppeteer from 'puppeteer';
+
+const delay = (time) => {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+}
+
+const browser = await puppeteer.launch({ headless: false, userDataDir: "./profile" });
+const page = await browser.newPage();
+
+/*
+FIRST RUN - UNCOMMENT AND SIGN IN
+
+await page.goto("https://venmo.com/account/sign-in")
+await delay(120000)
+*/
+
+// Select user to pay
+await page.goto("https://account.venmo.com/pay")
+await page.type("#search-input", "@Nicholas-Gebo-2");
+await (await page.waitForSelector("img[alt='Nicholas-Gebo-2']")).click()
+
+// Set up payment details
+await page.type("#payment-note", "🐜")
+await page.type("input[aria-label='Amount']", "0.01")
+await page.evaluate(() => {
+    document.querySelectorAll("button").forEach((button) => {
+        if (button.textContent === "Pay") {
+            button.click()
+        }
+    });
+});
+
+// Wait for payment methods to populate
+await page.waitForSelector("#select-destination");
+
+// Pay
+await page.evaluate(() => {
+    document.querySelectorAll("button").forEach((button) => {
+        console.log(button.textContent)
+        if (button.textContent === "Pay Nicholas Gebo $0.01") {
+            button.click()
+        }
+    });
+});
+
+// Cleanup
+await Promise.race([page.waitForNavigation(), delay(30000)]);
+await page.close();
+process.exit(0);
